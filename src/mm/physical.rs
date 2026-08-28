ba7e8c23ed70e2763a6279cc852286e478e46798
Novice <sharifmugisha06@gmail.com>
2026-08-28 19:20:03 +0200

    tried my best I guess

--- src/mm/physical.rs
+++ src/mm/physical.rs
@@ -11,21 +11,21 @@ unsafe impl Sync for Bitmap {}
 impl Bitmap {
     fn new(mem_map: &[LimineMemMapEntry], limine_hhdm_offset: u64, total_page_number: u32) -> Self {
 
-        let length = (total_page_number + 7) / 8;
+        let bitmap_length = (total_page_number + 7) / 8;
 
         // we need to find the bitmap address within the memory map
         let bitmap_region = mem_map
         .iter()
         .filter(|region| region.etype == super::LIMINE_MEMMAP_USABLE)
-        .find(|region| region.length >= length as u64)
+        .find(|region| region.length >= bitmap_length as u64)
         .expect("there is no way we didnt find any suitable region for the bitmap");
 
-        let mut bitmap = Bitmap { data: (bitmap_region.base + limine_hhdm_offset) as *mut u8, length };
+        let mut bitmap = Bitmap { data: (bitmap_region.base + limine_hhdm_offset) as *mut u8, length: bitmap_length };
 
-        println!("bitmap base: 0x{:x} length {}", bitmap_region.base, length);
+        println!("bitmap base: 0x{:x} length {}", bitmap_region.base, bitmap_length);
 
         // initially we mark the whole physical address space as used
-        bitmap.sliced_bitmap_mut().fill(0);
+        bitmap.sliced_bitmap_mut().fill(0xFF);
 
         // then first we map free regions
         let available_regions = mem_map
@@ -53,7 +53,7 @@ impl Bitmap {
         }
 
         // after all that we need to map the bitmap region as used
-        for addr in (bitmap_region.base..bitmap_region.base + length as u64).step_by(0x1000) {
+        for addr in (bitmap_region.base..bitmap_region.base + bitmap_length as u64).step_by(0x1000) {
             bitmap.set(Bitmap::addr_to_index(addr));
         }
 
@@ -107,10 +107,10 @@ impl Bitmap {
         index as u64 * 0x1000
     }
 
-    fn search_free_ranges(&self, count: usize) -> Option<usize> {
+    fn search_free_ranges(&self, count: usize, total_page_number: u32) -> Option<usize> {
         let mut range: usize = 0;
 
-        for index in 0..self.length as usize {
+        for index in 0..total_page_number as usize {
             if !self.is_used(index) {
                 range += 1;
             } else {
@@ -127,8 +127,69 @@ impl Bitmap {
 
 }
 
+struct FreePage {
+    next: *mut FreePage
+}
+
+struct FreeList {
+    head: *mut FreePage
+}
+
+unsafe impl Send for FreeList {}
+unsafe impl Sync for FreeList {}
+
+impl FreeList {
+    fn from_bitmap(bitmap: &Bitmap, total_pages: u32) -> Self {
+        let mut list = FreeList {
+            head: core::ptr::null_mut()
+        };
+
+        // initializing the head
+        let first_free = bitmap.search_free_ranges(1, total_pages).unwrap();
+        list.head = Bitmap::index_to_addr(first_free) as *mut FreePage;
+        unsafe {(*list.head).next = core::ptr::null_mut()};
+
+        let mut tail= list.head;
+        
+        for index in first_free+1..=total_pages as usize {
+            if bitmap.is_used(index) {continue;}
+
+            unsafe {
+                let new_tail = Bitmap::index_to_addr(index) as *mut FreePage;
+                (*new_tail).next = core::ptr::null_mut();
+
+                (*tail).next = new_tail;
+                tail = new_tail;
+            }
+        }
+
+        list
+    }
+
+    fn get(&mut self) -> *mut u8 {
+        let ret = self.head;
+
+        if !self.head.is_null() {
+            unsafe {
+                self.head = (*self.head).next;
+            }
+        }
+
+        ret as *mut u8
+    }
+
+    fn put(&mut self, addr: *mut u8) {
+        let head = addr as *mut FreePage;
+        unsafe {(*head).next = self.head}
+
+        self.head = head;
+
+    }
+}
+
 pub struct PhysMemAllocator {
     bitmap: Option<Bitmap>,
+    free_list: Option<FreeList>,
     total_pages: u32,
     free_pages: u32,
     used_pages: u32,
@@ -138,6 +199,7 @@ impl PhysMemAllocator {
     const fn new() -> Self {
         Self {
             bitmap: None,
+            free_list: None,
             total_pages: 0,
             free_pages: 0,
             used_pages: 0,
@@ -145,27 +207,59 @@ impl PhysMemAllocator {
     }
 
     fn alloc_page(&mut self) -> *mut u8 {
-        let free_index = self.bitmap
-        .as_ref()
-        .unwrap()
-        .search_free_ranges(1);
+        if let Some(free_list) = self.free_list.as_mut() {
+            if self.free_pages <= 0 {return core::ptr::null_mut();}
 
-        if let Some(index) = free_index {
-            self.bitmap
-            .as_mut()
-            .unwrap()
-            .set(index);
+            // let free_list = self.free_list.as_mut().unwrap();
+            let bitmap = self.bitmap.as_mut().unwrap();
 
-            self.used_pages += 1;
-            self.free_pages -= 1;
+            loop {
+                let addr = free_list.get();
+                if bitmap.is_used(Bitmap::addr_to_index(addr as u64)) {
+                    // this means that we probably allocated this page using only the bitmap
+                    // or somehow a used page got placed in the free list by error (I'm only a human after all)
+                    continue;   // anyway retry !! but seriously I need to panic here or atleast fallback to only use the bitmap
+                }
 
-            Bitmap::index_to_addr(index) as *mut u8
+                self.used_pages += 1;
+                self.free_pages -= 1;
+
+                bitmap.set(Bitmap::addr_to_index(addr as u64)); // set this page as used
+                break addr;
+            }
         } else {
-            core::ptr::null_mut()
+            let free_index = self.bitmap
+            .as_ref()
+            .unwrap()
+            .search_free_ranges(1, self.total_pages);
+
+            if let Some(index) = free_index {
+                self.bitmap
+                .as_mut()
+                .unwrap()
+                .set(index);
+
+                self.used_pages += 1;
+                self.free_pages -= 1;
+
+                Bitmap::index_to_addr(index) as *mut u8
+            } else {
+                core::ptr::null_mut()
+            }
         }
     }
 
     fn free_page(&mut self, addr: *mut u8) {
+
+        // self.free_list
+        // .as_mut()
+        // .unwrap()
+        // .put(addr);
+
+        if let Some(free_list) = self.free_list.as_mut() {
+            free_list.put(addr);
+        }
+        
         self.bitmap
         .as_mut()
         .unwrap()
@@ -182,6 +276,7 @@ pub fn init(memory_size: u32, mem_map_entries: &[LimineMemMapEntry], limine_hhdm
     let mut allocator = PHYSICAL_MEMORY_ALLOCATOR.lock();
     allocator.total_pages = (memory_size / 0x1000) as u32;
     allocator.bitmap = Some(Bitmap::new(mem_map_entries, limine_hhdm_offset,allocator.total_pages));
+    // allocator.free_list = Some(FreeList::from_bitmap(allocator.bitmap.as_ref().unwrap(), allocator.total_pages));
 
     for index in 0..allocator.total_pages {
         let bitmap = allocator.bitmap.as_ref().unwrap();
@@ -192,6 +287,10 @@ pub fn init(memory_size: u32, mem_map_entries: &[LimineMemMapEntry], limine_hhdm
         }
     }
 
+    println!("allocator, total pages {}, free pages {}, used pages {}", allocator.total_pages, allocator.free_pages, allocator.used_pages);
+
+    let page = allocator.alloc_page();
+    println!("test allocation: 0x{:x}", page as u64);
     println!("allocator, total pages {}, free pages {}, used pages {}", allocator.total_pages, allocator.free_pages, allocator.used_pages);
 
     let page = allocator.alloc_page();
